57ff0a0c9e67d0b10a9d48ff2b3aa0a9a011d2e3
hman <henrique.a.nunes@outlook.com>
2024-05-23 22:03:58 -0300

    Changed the code so its possible to transfer to an account that doesn't exist; Using the methods withdraw and deposit in the transfer method so there's no duplicated code;

--- services/account.py
+++ services/account.py
@@ -26,8 +26,10 @@ class Account:
 
         return DICT_BANK[self.account_id]['balance']
 
-    def deposit(self, amount: float) -> dict:
-        if not self.account_id:
+    def deposit(self, amount: float, destination_id: str = None) -> dict:
+        destination_id = destination_id or self.account_id
+
+        if not destination_id:
             raise CustomHttpException(status_code=status.HTTP_400_BAD_REQUEST,
                                       content={'message': 'Account ID is required'})
 
@@ -35,18 +37,18 @@ class Account:
             raise CustomHttpException(status_code=status.HTTP_400_BAD_REQUEST,
                                       content={'message': 'Amount must be greater than 0'})
 
-        if self.account_id not in DICT_BANK:
-            DICT_BANK[self.account_id] = {
+        if destination_id not in DICT_BANK:
+            DICT_BANK[destination_id] = {
                 'balance': amount,
             }
 
         else:
-            DICT_BANK[self.account_id]['balance'] += amount
+            DICT_BANK[destination_id]['balance'] += amount
 
         response = {
             'destination': {
-                'id': self.account_id,
-                'balance': DICT_BANK[self.account_id]['balance']
+                'id': destination_id,
+                'balance': DICT_BANK[destination_id]['balance']
             }
         }
 
@@ -93,26 +95,11 @@ class Account:
             raise CustomHttpException(status_code=status.HTTP_404_NOT_FOUND,
                                       content='0')
 
-        if destination_id not in DICT_BANK:
-            raise CustomHttpException(status_code=status.HTTP_404_NOT_FOUND,
-                                      content='0')
-
         if amount > DICT_BANK[self.account_id]['balance']:
             raise CustomHttpException(status_code=status.HTTP_400_BAD_REQUEST,
                                       content={'message': 'Insufficient funds'})
 
-        DICT_BANK[self.account_id]['balance'] -= amount
-        DICT_BANK[destination_id]['balance'] += amount
+        withdraw_response = self.withdraw(amount=amount)
+        deposit_response = self.deposit(amount=amount, destination_id=destination_id)
 
-        response = {
-            'origin': {
-                'id': self.account_id,
-                'balance': DICT_BANK[self.account_id]['balance']
-            },
-            'destination': {
-                'id': destination_id,
-                'balance': DICT_BANK[destination_id]['balance']
-            }
-        }
-
-        return response
+        return {**withdraw_response, **deposit_response}
